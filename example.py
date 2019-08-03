9d99e2605f80260012709fde0c2d072890adf2c4
Lucy Parry <lucy.parry@gmail.com>
2019-08-03 20:59:53 +0000

    Quick changes to update to Python 3

--- example.py
+++ example.py
@@ -12,12 +12,12 @@ else:
     from emulated_hx711 import HX711
 
 def cleanAndExit():
-    print "Cleaning..."
+    print( "Cleaning...")
 
     if not EMULATE_HX711:
         GPIO.cleanup()
         
-    print "Bye!"
+    print("Bye!")
     sys.exit()
 
 hx = HX711(5, 6)
@@ -37,13 +37,13 @@ hx.set_reading_format("MSB", "MSB")
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
 #hx.set_reference_unit(113)
-hx.set_reference_unit(1)
+hx.set_reference_unit(570)
 
 hx.reset()
 
 hx.tare()
 
-print "Tare done! Add weight now..."
+print("Tare done! Add weight now...")
 
 # to use both channels, you'll need to tare them both
 #hx.tare_A()
@@ -53,21 +53,21 @@ while True:
     try:
         # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
         # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
+        # Comment the two lines "val = hx.get_weight(5)" and "print( val" and uncomment these three lines to see what it print(s.
         
         # np_arr8_string = hx.get_np_arr8_string()
         # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
+        # print( binary_string + " " + np_arr8_string
         
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+        # print(s the weight. Comment if you're debbuging the MSB and LSB issue.
         val = hx.get_weight(5)
-        print val
+        print(val)
 
         # To get weight from both channels (if you have load cells hooked up 
         # to both channel A and B), do something like this
         #val_A = hx.get_weight_A(5)
         #val_B = hx.get_weight_B(5)
-        #print "A: %s  B: %s" % ( val_A, val_B )
+        #print( "A: %s  B: %s" % ( val_A, val_B )
 
         hx.power_down()
         hx.power_up()
